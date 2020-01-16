9a645d5709115530d38228042c575b9ad281ee74
Luciano Pessoa <luciano.pessoa@fonnet.com.br>
2020-01-16 11:05:20 -0300

    arquivo app inicial importante para a aplicacao

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const app = express()
+
+// Gateway padrão
+const port = 7000; 
+
+// Conecção com o banco localhost
+const connStr = "Server=https://191.252.59.59;Database=FonNetSite;User ID=site_view;Password=@Fnt2020!;";
+const sql = require('mssql');
+
+sql.connect(connStr)
+    .then(conn => global.conn = conn)
+    .catch(err => console.log(err));
+
+// Monta o json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Rota inicial
+const router = express.Router();
+router.get('/',(req ,res) => res.json({
+    message:'API Fonnet funcionando'
+}));
+app.use('/',router);
+
+
+//Rota de consulta
+router.get('/api-fonnet-wordpress',(req ,res) => {
+    execSQLQuery('SELECT * FROM TB_Produtos',res);
+    //execSQLQuery("SELECT ID, COD_PROD,PARTNUMBER, DESCRICAO, TIPO, PesoLiquido, FABRICANTE,PN_COMPATIBILIDADE, Especificacoes, FAMILIA_PLATAFORMA, Foto,EstoqueMinimo, DescricaoProjEspecial, DescricaoEtiqueta, DetalhesSite, CATEGORIA FROM TB_Produtos",res);
+})
+
+// Função Query de consulta
+function execSQLQuery(sqlQry,res){
+    global.conn.request()
+                .query(sqlQry)
+                .then(result => res.json(result.recordset))
+                .catch(err => res.json(err))
+}
+
+app.listen(port, () => console.log(`API funcionando sem erro`))
